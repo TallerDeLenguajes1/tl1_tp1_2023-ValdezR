039c0e1cfa468f4ec1d59ceab6cddc622d84ffda
Ramon <valdez.ramonh@gmail.com>
2023-03-23 19:19:47 -0300

    segundo commit

--- tp1_1.c
+++ tp1_1.c
@@ -3,5 +3,18 @@
 
 void main()
 {
-    printf("Hola mundo");
+    printf("Hola mundo\n");
+    int random = 30;
+    int * pRandom = &random;
+    //punto 1
+    printf("%p\n", pRandom);
+    //punto 2
+    printf("%p\n", pRandom);
+    //punto 3
+    printf("%p\n", &random);
+    //punto 4
+    printf("%p\n", &pRandom);
+    //punto 5
+    printf("%d\n", sizeof(random));
+
 }
